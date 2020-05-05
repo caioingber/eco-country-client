be15d51627e0071abd9597c8e7d451fa4e05c0fc
caio_ingber <caioingber@gmail.com>
2020-05-04 22:39:31 -0500

    can access data at all routes

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -7,8 +7,11 @@ export default {
   props: ["info"],
   name: "Country",
   computed: {
-    countryData() {
-      return this.$store.state.countries;
+    country() {
+      let filtered = this.$store.state.countries.filter(
+        (country) => country.country.name == this.$route.params.country
+      );
+      return filtered[0];
     },
   },
 };
